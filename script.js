ba0cff35d18add656e526b3bc66b7a261c9de4df
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-10 19:43:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 function convertToRoman(num) {
-  const symbols = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
-
-  let roman = '';
-
-  for (let i = 0; i < symbols.length; i++) {
-    const [symbol, value] = symbols[i];
-    while (num >= value) {
-      roman += symbol;
-      num -= value;
-    }
-  }
-
-  return roman;
+  
 }
 
 // Test cases
